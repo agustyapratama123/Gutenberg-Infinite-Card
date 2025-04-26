0630e2e16b4c2013cb4432c98738d03ac20e2ace
Agustya Pratama <agustyapratama123@gmail.com>
2025-04-26 21:23:41 +0700

    perbaikan jika di editor input card kosong maka jangan tampilkan di frontend

--- build/my-infinite-card/index.asset.php
+++ build/my-infinite-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-primitives'), 'version' => '828924181bfa7175982c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-primitives'), 'version' => '04ae2057e245749b9fd7');
--- src/my-infinite-card/save.js
+++ src/my-infinite-card/save.js
@@ -6,14 +6,17 @@ export default function save({ attributes }) {
   return (
     <div {...useBlockProps.save({ className: 'card-list-wrapper' })}>
       <div className="card-list">
-        {cards.map((card, index) => (
-          <div key={index} className="card-item">
-            {card.imageUrl && <img src={card.imageUrl} alt="" />}
-            <RichText.Content tagName="p" value={card.description} />
-          </div>
-        ))}
+        {cards
+          .filter((card) => card.imageUrl || (card.description && card.description.trim() !== ''))
+          .map((card, index) => (
+            <div key={index} className="card-item">
+              {card.imageUrl && <img src={card.imageUrl} alt="" />}
+              {card.description && (
+                <RichText.Content tagName="p" value={card.description} />
+              )}
+            </div>
+          ))}
       </div>
     </div>
   );
 }
-
